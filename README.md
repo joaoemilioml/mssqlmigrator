81a3a53bf1732f24b4a199bd825851d618bae272
João Emílio Medina Lages <joao.lages@radixeng.com>
2021-01-12 20:36:35 -0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # mssqlmigrator
+
+.NET Console App tool to automatically run migrations in SQL Server Database. This can be used in DevOps pipelines to automatically apply migration scripts to target environments.
+
+Inspired by: [dbmate](https://github.com/amacneil/dbmate)
+
+### Usage
+
+- Use Visual Studio to Publish the solution to a [single file](https://docs.microsoft.com/pt-br/dotnet/core/deploying/single-file)
+- run mssqlmigrator.exe new to create a new migration file
+- Fill the generated file's up and down section. The up section will be executed upon the migration's application and the down section will be executed to revert the migration. The file needs to keep the name pattern "yyyyMMddHHmmssff_migration" where "migration" can be replaced by a more suitable name. The date id and the underscore need to be maintained.
+- Fill the config.json with the connectionStrings of each environment to which you would like to apply the migrations
+- Create a migration table in the target database, containing only one column named id, of type varchar(100)
+- run "mssqlmigrator.exe up [env]" to apply all the migrations that are on the migration folder that have not yet been applied on the target database. If no env is specified, this option defaults to "dev".
+- run "mssqlmigrator.exe down [env] [migrationId]" to revert a single migration. If env is not specified, it defaults to "dev". If migrationId is not specified, it defaults to the last migration applied to the database.
+  
+### Dependencies: 
+- .NET Core 3
+- Dapper
+- NewtonSoft.Json
+- System.Data.SqlClient
---
+++ mssqlmigrator/mssqlmigrator/Program.cs
@@ -0,0 +1,155 @@
+﻿using Dapper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace mssqlmigrator
+{
+    class Program
+    {
+        private static string env;
+        
+        static async Task Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                PrintOptions();
+                return;
+            }
+
+            (string option, string migrationId) = GetOptions(args);
+
+            switch (option)
+            {
+                case "new":
+                    CreateMigrationFile();
+                    break;
+                case "up":
+                    await MigrateUp();
+                    break;
+                case "down":
+                    await MigrateDown(migrationId);
+                    break;
+                default:
+                    Console.WriteLine("Invalid command");
+                    PrintOptions();
+                    break;
+            }
+        }
+
+        private static (string, string) GetOptions(string[] args)
+        {
+            var option = args[0].ToLowerInvariant();
+            env = args.ElementAtOrDefault(1) ?? "dev";
+            var migrationId = args.ElementAtOrDefault(2) ?? string.Empty;
+            return (option, migrationId);
+        }
+
+        private static async Task MigrateDown(string migrationId)
+        {
+            bool up = false;
+            await ExecuteMigrationUpOrDown(up, migrationId);
+        }
+
+        private static async Task MigrateUp()
+        {
+            bool up = true;
+            await ExecuteMigrationUpOrDown(up);
+
+        }
+
+        private static async Task ExecuteMigrationUpOrDown(bool up, string migrationId = "")
+        {
+            string sqlConnectionString = GetConnectionString();
+
+            var files = Directory.GetFileSystemEntries("migration", "*.sql").Select(x => Path.GetFileName(x)).OrderBy(x=> x);
+
+            using SqlConnection conn = new SqlConnection(sqlConnectionString);
+            await conn.OpenAsync();
+            var tran = await conn.BeginTransactionAsync();
+            var fileIds = files.ToDictionary(x =>x.Split("_").First(), x => x);
+            var migrations = await conn.QueryAsync<string>("SELECT id FROM migration order by id", null, tran);
+
+            if (up)
+            {
+                var newMigrations = fileIds.Where(x => !migrations.Contains(x.Key));
+               
+                foreach (var file in newMigrations)
+                {
+                    Console.WriteLine($"Applying {file.Key}");
+                    string fullScript = File.ReadAllText(Path.Combine("migration", file.Value));
+                    var scripts = fullScript.Split("--down");
+                    var script = scripts.First();
+                    await conn.ExecuteAsync(script, transaction:tran);
+                }
+                var migrationsToInsert = newMigrations.Select(x => new { id = x.Key });
+                
+                if (migrationsToInsert?.Any() == true)
+                {
+                    await conn.ExecuteAsync("INSERT INTO migration (id) VALUES (@id)", migrationsToInsert, tran);
+                }
+                
+            }
+            else
+            {
+                var migrationToUndo = migrations.FirstOrDefault(x => x == migrationId) ?? migrations.Last();
+                var migrationFileToUndo = fileIds.FirstOrDefault(x => x.Key == migrationToUndo);
+                if (!migrationFileToUndo.Equals(default(KeyValuePair<string, string>)))
+                {
+                    Console.WriteLine($"Rolling back {migrationFileToUndo.Key}");
+                    string fullScript = File.ReadAllText(Path.Combine("migration", migrationFileToUndo.Value));
+                    var scripts = fullScript.Split("--down");
+                    var script = scripts.Last();
+                    await conn.ExecuteAsync(script, transaction:tran);
+                    await conn.ExecuteAsync("DELETE FROM migration WHERE id = @id", new { id = migrationFileToUndo.Key }, tran);
+                }
+            }
+
+            await tran.CommitAsync();
+            await conn.CloseAsync();
+
+
+        }
+
+        private static string GetConnectionString()
+        {
+            var configStr = File.ReadAllText("config.json");
+            var configObj = JsonConvert.DeserializeObject<Dictionary<string, string>>(configStr);
+
+            if (!configObj.Keys.Contains(env))
+            {
+                Console.WriteLine("Invalid environment");
+                throw new ArgumentException("env");
+            }
+
+            string sqlConnectionString = configObj[env];
+            return sqlConnectionString;
+        }
+
+        private static void CreateMigrationFile()
+        {
+            var dateNow = DateTime.UtcNow;
+            var asm = Assembly.GetExecutingAssembly();
+            //using Stream stream = asm.GetManifestResourceStream("mssqlmigrator.template_migration.sql");
+            var dir = Directory.CreateDirectory("migration");
+            //using StreamReader streamReader = new StreamReader(stream);
+            var fileContent = File.ReadAllText("template_migration.sql");
+            //string fileContent =  streamReader.ReadToEnd();
+            File.WriteAllText(Path.Combine(dir.FullName, $"{dateNow:yyyyMMddHHmmssff}_migration.sql"), fileContent);
+
+        }
+
+        private static void PrintOptions()
+        {
+            Console.WriteLine("Please enter one of the following options:");
+            Console.WriteLine("- 'new' to create a new migration file");
+            Console.WriteLine("- 'up' to apply new migrations");
+            Console.WriteLine("- 'down' to rollback latest migration");
+        }
+    }
+}
